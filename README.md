d21dcac62dfffcd4bfffb8e82a24b4e68d6e2152
root <Joe Daly>
2018-07-11 15:36:24 +0000

    added packages necessary for running tbm2 to the hearts_vision repository

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # hearts_vision
 Machine vision related packages, etc
+
+
+
+11.7.18
+
+The packages: hearts_face_uniform, hearts_person_tracker are used with tbm2_welcoming_visitors.launch
---
+++ hearts_face_uniform/src/macros.hpp
@@ -0,0 +1,63 @@
+
+
+#define CAPTURE_NUM 30
+
+#define BLUE Scalar(255, 100, 60)
+#define GREEN Scalar(70, 230, 70)
+#define RED Scalar(70, 70, 255)
+#define AMBER Scalar(0, 194, 255)
+#define INSTRUCT Scalar(255, 150, 50)
+#define BLACK Scalar(0,0,0)
+#define WHITE Scalar(200, 200, 200)
+
+#define MIN_FACE_SIZE 70
+#define MAX_FACE_SIZE 300
+
+#define CONFIDENCE_THRESH 180
+
+#define THRESHOLD 50
+
+//#define POSTMAN_B_MEAN 59
+//#define POSTMAN_G_MEAN 213
+//#define POSTMAN_R_MEAN 199
+//#define POSTMAN_B_STDEV 17
+//#define POSTMAN_G_STDEV 37
+//#define POSTMAN_R_STDEV 45
+
+//#define DELIMAN_B_MEAN 221
+//#define DELIMAN_G_MEAN 212
+//#define DELIMAN_R_MEAN 189
+//#define DELIMAN_B_STDEV 42
+//#define DELIMAN_G_STDEV 28
+//#define DELIMAN_R_STDEV 41
+
+#define POSTMAN_B_MEAN 60
+#define POSTMAN_G_MEAN 200
+#define POSTMAN_R_MEAN 200
+#define POSTMAN_B_STDEV 25
+#define POSTMAN_G_STDEV 50
+#define POSTMAN_R_STDEV 55
+
+#define DELIMAN_B_MEAN 220
+#define DELIMAN_G_MEAN 200
+#define DELIMAN_R_MEAN 160
+#define DELIMAN_B_STDEV 55
+#define DELIMAN_G_STDEV 55
+#define DELIMAN_R_STDEV 58
+
+#define POSTMAN_RG_MIN 0.93
+#define POSTMAN_RG_MAX 1.03
+#define POSTMAN_RB_MIN 2.2
+#define POSTMAN_RB_MAX 2.6
+#define POSTMAN_GB_MIN 2.2
+#define POSTMAN_GB_MAX 2.6
+
+#define DELIMAN_RG_MIN 0.86
+#define DELIMAN_RG_MAX 0.96
+#define DELIMAN_RB_MIN 0.77
+#define DELIMAN_RB_MAX 0.87
+#define DELIMAN_GB_MIN 0.85
+#define DELIMAN_GB_MAX 0.95
+
+#define NOT_DOCTOR_COUNT 5
+#define UNKNOWN_COUNT 200
