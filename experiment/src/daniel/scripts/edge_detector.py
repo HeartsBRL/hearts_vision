cfa5173db0a7a0612409eccb34a095fb36f1f202
derek <derek@firstcottage.com>
2018-08-01 15:02:17 +0100

    colour detection & edge detection

---
+++ experiment/src/daniel/scripts/edge_detector.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import argparse
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image")
+args = vars(ap.parse_args())
+
+# load the image
+img = cv2.imread(args["image"])
+
+edges = cv2.Canny(img,100,200)
+
+plt.subplot(121),plt.imshow(img,cmap = 'gray')
+plt.title('Original Image'), plt.xticks([]), plt.yticks([])
+plt.subplot(122),plt.imshow(edges,cmap = 'gray')
+plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
+plt.show()
