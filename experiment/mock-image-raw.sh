fdc63413003f2d4c0be4b32f3c86e252d451ba6c
stevebattle <steve.battle@uwe.ac.uk>
2018-08-14 17:07:35 +0100

    added false negative test image

--- experiment/mock-image-raw.sh
+++ experiment/mock-image-raw.sh
@@ -4,4 +4,4 @@
 # ./mock-image-raw.sh
 
 source devel/setup.bash
-rosrun steve mock-image-raw.py 'photos/cv - 5.jpg' 'photos/cv - 6.jpg' 'photos/cv - 7.jpg' 'photos/cv - 8.jpg'
+rosrun steve mock-image-raw.py 'photos/cv - 5.jpg' 'photos/cv - 6.jpg' 'photos/cv - 7.jpg' 'photos/cv - 8.jpg' 'photos/cv - 9.png'
